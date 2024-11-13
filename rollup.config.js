5d60276908b1c25a7e6a0b4dd040160f27105388
Kim Meyer <kim.meyer@palasthotel.de>
2024-11-13 12:22:08 +0100

    Use tabs instead of spaces

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import minifyHTML from "rollup-plugin-minify-html-literals";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/fs-scrollable.js',
-  output: {
-    dir: 'build',
-    format: 'es',
-  },
-  plugins: [
-    minifyHTML(),
-    resolve(),
-    production && terser(), // Minify, but only in production
-  ],
-  preserveEntrySignatures: false,
+	input: "src/fs-scrollable.js",
+	output: {
+		dir: "build",
+		format: "es",
+	},
+	plugins: [
+		minifyHTML(),
+		resolve(),
+		production && terser(), // Minify, but only in production
+	],
+	preserveEntrySignatures: false,
 };
